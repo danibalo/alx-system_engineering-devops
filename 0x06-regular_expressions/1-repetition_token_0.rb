2e31cc98c3febbc4df9b19bb4f0a69da2c0fc8cb
danibalo <daniyemak@gmail.com>
2023-02-02 21:45:28 -0800

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
-puts ARGV[0].scan(/School/).join
+# A regular expression that is matches a given pattern
+puts ARGV[0].scan(/hbt{2,5}n/).join
